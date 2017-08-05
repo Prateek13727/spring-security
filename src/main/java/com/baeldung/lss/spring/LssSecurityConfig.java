cd52e051c68eea924a27e5649f9adaea09a9a296
Prateek13727 <prateek.pandey@tracxn.com>
2017-08-05 23:34:49 +0530

    [rememberMe] persistent token functionality

--- src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,17 +1,26 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private DataSource dataSource;
 
     public LssSecurityConfig() {
         super();
@@ -45,11 +54,21 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
             loginProcessingUrl("/doLogin")
 
         .and()
+        .rememberMe().tokenRepository(persistentTokenRepository())
+
+        .and()
         .logout().permitAll().logoutUrl("/logout")
 
         .and()
         .csrf().disable()
         ;
     } // @formatter:on
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        final JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
 
 }
